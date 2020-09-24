e51b590a18996dd2c8e2b35512e858560df8a558
George <subin.george@nokia.com>
2020-09-24 18:10:43 +0530

    source removed

--- source/rotate_array.py
+++
@@ -1,9 +0,0 @@
-tests = int(input())
-
-for test in range(tests):
-    n, d = map(int, (input().split(' ')))
-    _input = input()
-    array = list(map(int, _input.split()))
-        
-    print(*array[d:] + array[:d])
-        
